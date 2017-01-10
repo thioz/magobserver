de6570652b7efd4455fd46b6538695a85d87bdb2
Rob Theeuwes <dev@sourcerer.nl>
2017-01-11 00:41:04 +0100

    added configurable rules

--- app/code/local/Gracious/ProdImport/Model/Observer.php
+++ app/code/local/Gracious/ProdImport/Model/Observer.php
@@ -1,7 +1,5 @@
 <?php
 
-/* Our class name should follow the directory structure of our Observer.php model, starting from the namespace, replacing directory separators with underscores. The directory of ousr Observer.php is following:
-  app/code/local/Mage/ProductLogUpdate/Model/Observer.php */
 
 class Gracious_ProdImport_Model_Observer {
 	// Magento passes a Varien_Event_Observer object as the first parameter of dispatched events.
